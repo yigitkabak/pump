a4bed1055a52995538f3850f28100145e263350b
Yiğit KABAK <yigitkabak@proton.me>
2025-04-20 14:19:10 +0300

    Create color.go

---
+++ __TEST__/constants/color.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
+	ColorCyan   = "\033[36m"
+	ColorGray   = "\033[90m"
+	ColorBold   = "\033[1m"
+)
